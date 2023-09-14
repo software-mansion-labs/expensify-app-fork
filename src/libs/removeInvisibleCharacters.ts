02875bf68f1eb0ced5cb6c87e1a457ae9975c956
Jakub Kosmydel <104823336+kosmydel@users.noreply.github.com>
2023-09-14 07:40:30 +0200

    use validation utils in workspace name

--- src/libs/removeInvisibleCharacters.ts
+++ src/libs/removeInvisibleCharacters.ts
@@ -8,14 +8,14 @@ function removeInvisibleCharacters(value: string): string {
     // - \u200B: zero-width space
     // - \u00A0: non-breaking space
     // - \u2060: word joiner
-    result = result.replace(/[\u200B\u00A0\u2060]/g, '')
+    result = result.replace(/[\u200B\u00A0\u2060]/g, '');
 
     // Remove all characters from the 'Other' (C) category except for format characters (Cf)
     // because some of them they are used for emojis
     result = result.replace(/[\p{Cc}\p{Cs}\p{Co}\p{Cn}]/gu, '');
 
     // Remove characters from the (Cf) category that are not used for emojis
-    result = result.replace(/[\u200E-\u200F]/g, '')
+    result = result.replace(/[\u200E-\u200F]/g, '');
 
     // Remove all characters from the 'Separator' (Z) category except for Space Separator (Zs)
     result = result.replace(/[\p{Zl}\p{Zp}]/gu, '');
