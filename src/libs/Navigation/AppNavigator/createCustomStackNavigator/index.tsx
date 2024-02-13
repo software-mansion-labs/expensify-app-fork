00be6b483d2d99365401164411c7028fd77e9bd9
Adam Grzybowski <adam.grzybowski@swmansion.com>
2024-02-13 18:52:44 +0100

    recycle report screens initial

--- src/libs/Navigation/AppNavigator/createCustomStackNavigator/index.tsx
+++ src/libs/Navigation/AppNavigator/createCustomStackNavigator/index.tsx
@@ -6,21 +6,49 @@ import React, {useEffect, useMemo} from 'react';
 import useWindowDimensions from '@hooks/useWindowDimensions';
 import navigationRef from '@libs/Navigation/navigationRef';
 import NAVIGATORS from '@src/NAVIGATORS';
+import SCREENS from '@src/SCREENS';
 import CustomRouter from './CustomRouter';
 import type {ResponsiveStackNavigatorProps, ResponsiveStackNavigatorRouterOptions} from './types';
 
 type Routes = StackNavigationState<ParamListBase>['routes'];
-function reduceReportRoutes(routes: Routes): Routes {
+
+// We want to reduce how much central pane screens are rendered to improve performance
+function reduceAndRecycleRoutes(routes: Routes): Routes {
     const result: Routes = [];
-    let count = 0;
+
+    // Counters for central pane screens.
+    let reportCount = 0;
+    let nonReportCentralPaneCount = 0;
     const reverseRoutes = [...routes].reverse();
 
+    // We have separete batch of central pane screens for reports and non-reports.
+    // This way switching between tabs should be faster because we always have some screens mounted.
     reverseRoutes.forEach((route) => {
-        if (route.name === NAVIGATORS.CENTRAL_PANE_NAVIGATOR) {
-            // Remove all report routes except the last 3. This will improve performance.
-            if (count < 3) {
+        if (route.name === NAVIGATORS.CENTRAL_PANE_NAVIGATOR && route.params && 'screen' in route.params && 'params' in route.params && route.params.screen === SCREENS.REPORT) {
+            if (reportCount < 3) {
+                const oldestSameParamsRoute = routes.find(
+                    (r) =>
+                        r.name === NAVIGATORS.CENTRAL_PANE_NAVIGATOR &&
+                        r.params &&
+                        'screen' in r.params &&
+                        r.params.screen === SCREENS.REPORT &&
+                        'params' in r.params &&
+                        r.params.params &&
+                        r.params.params.reportID === route.params.params.reportID,
+                );
+
+                if (oldestSameParamsRoute && !result.includes(oldestSameParamsRoute)) {
+                    reportCount++;
+                    result.push(oldestSameParamsRoute);
+                } else if (!oldestSameParamsRoute) {
+                    reportCount++;
+                    result.push(route);
+                }
+            }
+        } else if (route.name === NAVIGATORS.CENTRAL_PANE_NAVIGATOR && route.params && 'screen' in route.params && 'params' in route.params && route.params.screen !== SCREENS.REPORT) {
+            if (nonReportCentralPaneCount < 3) {
+                nonReportCentralPaneCount++;
                 result.push(route);
-                count++;
             }
         } else {
             result.push(route);
@@ -53,12 +81,14 @@ function ResponsiveStackNavigator(props: ResponsiveStackNavigatorProps) {
     }, [isSmallScreenWidth]);
 
     const stateToRender = useMemo(() => {
-        const result = reduceReportRoutes(state.routes);
+        const routesToRender = reduceAndRecycleRoutes(state.routes);
+
+        console.log(routesToRender);
 
         return {
             ...state,
-            index: result.length - 1,
-            routes: [...result],
+            index: routesToRender.length - 1,
+            routes: [...routesToRender],
         };
     }, [state]);
 
