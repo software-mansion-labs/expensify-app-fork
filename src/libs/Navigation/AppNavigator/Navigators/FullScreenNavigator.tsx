ebea4b3c79f7862223a3e5567b724811cef22ba4
Wojciech Boman <wojciechboman@gmail.com>
2024-06-19 12:22:48 +0200

    Rename initialCentralPaneScreen to defaultCentralScreen

--- src/libs/Navigation/AppNavigator/Navigators/FullScreenNavigator.tsx
+++ src/libs/Navigation/AppNavigator/Navigators/FullScreenNavigator.tsx
@@ -35,7 +35,7 @@ function FullScreenNavigator() {
     return (
         <RootStack.Navigator
             sidebarScreen={SCREENS.WORKSPACE.INITIAL}
-            initialCentralPaneScreen={SCREENS.WORKSPACE.PROFILE}
+            defaultCentralScreen={SCREENS.WORKSPACE.PROFILE}
         >
             <RootStack.Screen
                 name={SCREENS.WORKSPACE.INITIAL}
