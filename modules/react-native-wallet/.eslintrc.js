ed399863ee53d515af07f508a64c5b1fe1b5210b
brunovjk <brunovjk@gmail.com>
2024-06-24 14:17:49 -0300

    fix type

---
+++ modules/react-native-wallet/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: './tsconfig.json',
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended'
+    ],
+    rules: {}
+  };
+  
