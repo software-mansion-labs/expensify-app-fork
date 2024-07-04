57b24ace0600e6970cc849c4ceef0f3905f6804b
brunovjk <brunovjk@gmail.com>
2024-07-04 09:01:05 -0300

    Darren120 Update ApplePushProvisioning.ts

--- modules/react-native-wallet/src/ApplePushProvisioning.ts
+++ modules/react-native-wallet/src/ApplePushProvisioning.ts
@@ -12,35 +12,50 @@ interface CompletePassRequest {
     encryptedData: string;
     ephemeralKey: string;
 }
-
+type SupportedEvents = 'addPaymentPassViewControllerDidFinish' | 'getPassAndActivation';
 const eventEmitter = new NativeEventEmitter(RNWallet);
 
 const ApplePushProvisioningModule = {
     async canAddPass(): Promise<boolean> {
         try {
-            return await RNWallet.canAddPass();
+            return await RNWallet.canAddPaymentPass();
         } catch {
             throw new Error(`Error checking add pass capability.`);
         }
     },
     async startAddPass(request: AddPassRequest): Promise<void> {
         try {
-            return await RNWallet.startAddPass(request.last4, request.cardHolder);
-        } catch {
-            throw new Error(`Error starting add pass.`);
+            return await RNWallet.startAddPaymentPass(request.last4, request.cardHolder);
+        } catch (e) {
+            throw e;
         }
     },
     async completeAddPass(request: CompletePassRequest): Promise<void> {
         try {
-            RNWallet.completeAddPass(request.activation, request.encryptedData, request.ephemeralKey);
-        } catch {
-            throw new Error(`Error completing add pass.`);
+            return await RNWallet.completeAddPaymentPass(request.activation, request.encryptedData, request.ephemeralKey);
+        } catch (e) {
+            throw e;
         }
     },
-    addEventListener(event: string, callback: (data: any) => void) {
+    addEventListener<T extends SupportedEvents>(
+        event: T,
+        callback: (
+            e: T extends 'getPassAndActivation'
+                ? {
+                      data: {
+                          certificateLeaf: string;
+                          certificateSubCA: string;
+                          nonce: string;
+                          nonceSignature: string;
+                      };
+                  }
+                : never,
+        ) => void,
+    ) {
         eventEmitter.addListener(event, callback);
     },
-    removeAllListeners(event: string) {
+
+    removeAllListeners(event: SupportedEvents) {
         eventEmitter.removeAllListeners(event);
     },
 };
