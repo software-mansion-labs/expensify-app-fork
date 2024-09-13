Expensify/App

Fix pressing settings tab when navigating deeper into workspaces

src/pages/home/sidebar/BottomTabAvatar.tsx
@@ -36,7 +36,13 @@ function BottomTabAvatar({isCreateMenuOpen = false, isSelected = false}: BottomT
             return;
         }
 
-        if ([SCREENS.SETTINGS.WORKSPACES, SCREENS.WORKSPACE.INITIAL].includes(route.name) && isSmallScreenWidth) {
+        if (route.name === SCREENS.SETTINGS.WORKSPACES && isSmallScreenWidth) {
+            Navigation.goBack(ROUTES.SETTINGS);
+            return;
+        }
+
+        if (route.name === SCREENS.WORKSPACE.INITIAL) {
+            Navigation.dismissModal();
             Navigation.goBack(ROUTES.SETTINGS);
             return;
         }

software-mansion-labs/expensify-app-fork

src/pages/home/sidebar/BottomTabAvatar.tsx
@@ -41,7 +41,13 @@ function BottomTabAvatar({isCreateMenuOpen = false, isSelected = false}: BottomT
             return;
         }
 
-        if (([SCREENS.SETTINGS.WORKSPACES, SCREENS.WORKSPACE.INITIAL] as string[]).includes(route.name) && shouldUseNarrowLayout) {
+        if (route.name === SCREENS.SETTINGS.WORKSPACES && shouldUseNarrowLayout) {
+            Navigation.goBack(ROUTES.SETTINGS);
+            return;
+        }
+
+        if (route.name === SCREENS.WORKSPACE.INITIAL) {
+            Navigation.dismissModal();
             Navigation.goBack(ROUTES.SETTINGS);
             return;
         }