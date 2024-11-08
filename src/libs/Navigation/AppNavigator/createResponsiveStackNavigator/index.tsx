18fdccdfda842316e0dbac47cec6162916a66792
Wojciech Boman <wojciechboman@gmail.com>
2024-11-08 14:48:45 +0100

    Add useCustomSplitNavigatorState and useCustomRouterState

--- src/libs/Navigation/AppNavigator/createResponsiveStackNavigator/index.tsx
+++ src/libs/Navigation/AppNavigator/createResponsiveStackNavigator/index.tsx
@@ -3,13 +3,24 @@ import {createNavigatorFactory} from '@react-navigation/native';
 import useNavigationResetOnLayoutChange from '@libs/Navigation/AppNavigator/useNavigationResetOnLayoutChange';
 import createPlatformStackNavigatorComponent from '@libs/Navigation/PlatformStackNavigation/createPlatformStackNavigatorComponent';
 import defaultPlatformStackScreenOptions from '@libs/Navigation/PlatformStackNavigation/defaultPlatformStackScreenOptions';
-import type {PlatformStackNavigationEventMap, PlatformStackNavigationOptions, PlatformStackNavigationState} from '@libs/Navigation/PlatformStackNavigation/types';
-import CustomRouter from '../createCustomStackNavigator/CustomRouter';
+import type {CustomStateHookProps, PlatformStackNavigationEventMap, PlatformStackNavigationOptions, PlatformStackNavigationState} from '@libs/Navigation/PlatformStackNavigation/types';
+import {isFullScreenName} from '@libs/NavigationUtils';
+import ReactComponentModule from '@src/types/utils/ReactComponentModule';
+
+const loadCustomRouter = require<ReactComponentModule>('@libs/Navigation/AppNavigator/createCustomStackNavigator/CustomRouter').default;
+
+function useCustomRouterState({state}: CustomStateHookProps) {
+    const lastSplitIndex = state.routes.findLastIndex((route) => isFullScreenName(route.name));
+    const routesToRender = state.routes.slice(Math.max(0, lastSplitIndex - 1), state.routes.length);
+
+    return {stateToRender: {...state, routes: routesToRender, index: routesToRender.length - 1}};
+}
 
 const ResponsiveStackNavigatorComponent = createPlatformStackNavigatorComponent('ResponsiveStackNavigator', {
-    createRouter: CustomRouter,
+    createRouter: loadCustomRouter,
     defaultScreenOptions: defaultPlatformStackScreenOptions,
     useCustomEffects: useNavigationResetOnLayoutChange,
+    useCustomState: useCustomRouterState,
 });
 
 function createResponsiveStackNavigator<ParamList extends ParamListBase>() {
--- src/libs/Navigation/AppNavigator/createSplitStackNavigator/index.tsx
+++ src/libs/Navigation/AppNavigator/createSplitStackNavigator/index.tsx
@@ -1,15 +1,32 @@
 import type {ParamListBase} from '@react-navigation/native';
 import {createNavigatorFactory} from '@react-navigation/native';
+import useResponsiveLayout from '@hooks/useResponsiveLayout';
 import useNavigationResetOnLayoutChange from '@libs/Navigation/AppNavigator/useNavigationResetOnLayoutChange';
 import createPlatformStackNavigatorComponent from '@libs/Navigation/PlatformStackNavigation/createPlatformStackNavigatorComponent';
 import defaultPlatformStackScreenOptions from '@libs/Navigation/PlatformStackNavigation/defaultPlatformStackScreenOptions';
-import type {PlatformStackNavigationEventMap, PlatformStackNavigationOptions, PlatformStackNavigationState} from '@libs/Navigation/PlatformStackNavigation/types';
+import type {CustomStateHookProps, PlatformStackNavigationEventMap, PlatformStackNavigationOptions, PlatformStackNavigationState} from '@libs/Navigation/PlatformStackNavigation/types';
 import SplitStackRouter from './SplitStackRouter';
+
+function useCustomSplitNavigatorState({state}: CustomStateHookProps) {
+    const {shouldUseNarrowLayout} = useResponsiveLayout();
+
+    const sidebarScreenRoute = state.routes.at(0);
+
+    if (!sidebarScreenRoute) {
+        return state;
+    }
+
+    const centralScreenRoutes = state.routes.slice(1);
+    const routesToRender = shouldUseNarrowLayout ? state.routes.slice(-2) : [sidebarScreenRoute, ...centralScreenRoutes.slice(-2)];
+
+    return {stateToRender: {...state, routes: routesToRender, index: routesToRender.length - 1}};
+}
 
 const CustomFullScreenNavigatorComponent = createPlatformStackNavigatorComponent('CustomFullScreenNavigator', {
     createRouter: SplitStackRouter,
     useCustomEffects: useNavigationResetOnLayoutChange,
     defaultScreenOptions: defaultPlatformStackScreenOptions,
+    useCustomState: useCustomSplitNavigatorState,
 });
 
 function createCustomFullScreenNavigator<ParamList extends ParamListBase>() {
