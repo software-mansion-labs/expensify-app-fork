15d362a5ce40811d5fd022f8733755e8b84b44ef
zfurtak <zuzkafurtak@gmail.com>
2024-11-13 11:40:30 +0000

    Created turbo module

---
+++ react-native-wallet/android/src/oldarch/WalletSpec.kt
@@ -0,0 +1,11 @@
+package com.wallet
+
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.Promise
+
+abstract class WalletSpec internal constructor(context: ReactApplicationContext) :
+  ReactContextBaseJavaModule(context) {
+
+  abstract fun multiply(a: Double, b: Double, promise: Promise)
+}
