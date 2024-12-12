369c2250eb82191f9c3bb61d34a0c8a0fd3badf8
Mateusz Rajski <mateusz.rajski@swmansion.com>
2024-12-12 15:26:42 +0100

    Adjust nitro contact PR to HybridApp

--- react-native.config.js
+++ react-native.config.js
@@ -1,5 +1,7 @@
 const iosSourceDir = process.env.PROJECT_ROOT_PATH ? process.env.PROJECT_ROOT_PATH + 'ios' : 'ios';
 const androidSourceDir = process.env.PROJECT_ROOT_PATH ? process.env.PROJECT_ROOT_PATH + 'android' : 'android';
+const path = require('path');
+const pak = require('./modules/ContactsNitroModule/package.json');
 
 module.exports = {
     project: {
@@ -7,4 +9,9 @@ module.exports = {
         android: {sourceDir: androidSourceDir},
     },
     assets: ['./assets/fonts/native'],
+    dependencies: {
+        [pak.name]: {
+            root: path.join(__dirname, 'modules', 'ContactsNitroModule'),
+        },
+    },
 };
