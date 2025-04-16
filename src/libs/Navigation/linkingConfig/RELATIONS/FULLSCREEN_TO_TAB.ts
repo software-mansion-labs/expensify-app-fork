Expensify/App

add WorksaceHubSplitNavigator

src/libs/Navigation/linkingConfig/RELATIONS/FULLSCREEN_TO_TAB.ts
@@ -7,6 +7,7 @@ const FULLSCREEN_TO_TAB: Record<FullScreenName, ValueOf<typeof BOTTOM_TABS>> = {
     [NAVIGATORS.REPORTS_SPLIT_NAVIGATOR]: BOTTOM_TABS.HOME,
     [NAVIGATORS.SEARCH_FULLSCREEN_NAVIGATOR]: BOTTOM_TABS.SEARCH,
     [NAVIGATORS.SETTINGS_SPLIT_NAVIGATOR]: BOTTOM_TABS.SETTINGS,
+    [NAVIGATORS.WORKSPACE_HUB_SPLIT_NAVIGATOR]: BOTTOM_TABS.WORKSPACES,
     [NAVIGATORS.WORKSPACE_SPLIT_NAVIGATOR]: BOTTOM_TABS.SETTINGS,
 };
 

software-mansion-labs/expensify-app-fork

src/libs/Navigation/linkingConfig/RELATIONS/FULLSCREEN_TO_TAB.ts
@@ -7,7 +7,8 @@ const FULLSCREEN_TO_TAB: Record<FullScreenName, ValueOf<typeof NAVIGATION_TABS>>
     [NAVIGATORS.REPORTS_SPLIT_NAVIGATOR]: NAVIGATION_TABS.HOME,
     [NAVIGATORS.SEARCH_FULLSCREEN_NAVIGATOR]: NAVIGATION_TABS.SEARCH,
     [NAVIGATORS.SETTINGS_SPLIT_NAVIGATOR]: NAVIGATION_TABS.SETTINGS,
-    [NAVIGATORS.WORKSPACE_SPLIT_NAVIGATOR]: NAVIGATION_TABS.SETTINGS,
+    [NAVIGATORS.WORKSPACE_HUB_SPLIT_NAVIGATOR]: NAVIGATION_TABS.WORKSPACES,
+    [NAVIGATORS.WORKSPACE_SPLIT_NAVIGATOR]: NAVIGATION_TABS.WORKSPACES,
 };
 
 export default FULLSCREEN_TO_TAB;