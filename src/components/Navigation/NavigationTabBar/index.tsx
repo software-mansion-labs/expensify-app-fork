Expensify/App

add workspaces tab

src/components/Navigation/BottomTabBar/index.tsx
@@ -254,13 +254,39 @@ function BottomTabBar({selectedTab, isTooltipAllowed = false}: BottomTabBarProps
                         {translate('common.reports')}
                     </Text>
                 </PressableWithFeedback>
+                <View style={[styles.flex1, styles.bottomTabBarItem]}>
+                    <BottomTabBarFloatingActionButton isTooltipAllowed={isTooltipAllowed} />
+                </View>
+                <PressableWithFeedback
+                    onPress={navigateToSearch}
+                    role={CONST.ROLE.BUTTON}
+                    accessibilityLabel={translate('common.workspaces')}
+                    style={styles.bottomTabBarItem}
+                >
+                    <View>
+                        <Icon
+                            src={Expensicons.Buildings}
+                            fill={theme.icon}
+                            width={variables.iconBottomBar}
+                            height={variables.iconBottomBar}
+                        />
+                    </View>
+                    <Text
+                        style={[
+                            styles.textSmall,
+                            styles.textAlignCenter,
+                            styles.mt1Half,
+                            selectedTab === BOTTOM_TABS.WORKSPACES ? styles.textBold : styles.textSupporting,
+                            styles.bottomTabBarLabel,
+                        ]}
+                    >
+                        {translate('common.workspaces')}
+                    </Text>
+                </PressableWithFeedback>
                 <BottomTabAvatar
                     isSelected={selectedTab === BOTTOM_TABS.SETTINGS}
                     onPress={showSettingsPage}
                 />
-                <View style={[styles.flex1, styles.bottomTabBarItem]}>
-                    <BottomTabBarFloatingActionButton isTooltipAllowed={isTooltipAllowed} />
-                </View>
             </View>
         </>
     );

software-mansion-labs/expensify-app-fork

src/components/Navigation/NavigationTabBar/index.tsx
@@ -288,6 +288,32 @@ function NavigationTabBar({selectedTab, isTooltipAllowed = false}: NavigationTab
                                 {translate('common.reports')}
                             </Text>
                         </PressableWithFeedback>
+                        <PressableWithFeedback
+                            onPress={navigateToSearch}
+                            role={CONST.ROLE.BUTTON}
+                            accessibilityLabel={translate('common.workspaces')}
+                            style={styles.leftNavigationTabBarItem}
+                        >
+                            <View>
+                                <Icon
+                                    src={Expensicons.Buildings}
+                                    fill={theme.icon}
+                                    width={variables.iconBottomBar}
+                                    height={variables.iconBottomBar}
+                                />
+                            </View>
+                            <Text
+                                style={[
+                                    styles.textSmall,
+                                    styles.textAlignCenter,
+                                    styles.mt1Half,
+                                    selectedTab === NAVIGATION_TABS.WORKSPACES ? styles.textBold : styles.textSupporting,
+                                    styles.navigationTabBarLabel,
+                                ]}
+                            >
+                                {translate('common.workspaces')}
+                            </Text>
+                        </PressableWithFeedback>
                         <NavigationTabBarAvatar
                             style={styles.leftNavigationTabBarItem}
                             isSelected={selectedTab === NAVIGATION_TABS.SETTINGS}
@@ -382,14 +408,41 @@ function NavigationTabBar({selectedTab, isTooltipAllowed = false}: NavigationTab
                         {translate('common.reports')}
                     </Text>
                 </PressableWithFeedback>
+                <View style={[styles.flex1, styles.navigationTabBarItem]}>
+                    <NavigationTabBarFloatingActionButton isTooltipAllowed={isTooltipAllowed} />
+                </View>
+                <PressableWithFeedback
+                    onPress={navigateToSearch}
+                    role={CONST.ROLE.BUTTON}
+                    accessibilityLabel={translate('common.workspaces')}
+                    wrapperStyle={styles.flex1}
+                    style={styles.navigationTabBarItem}
+                >
+                    <View>
+                        <Icon
+                            src={Expensicons.Buildings}
+                            fill={theme.icon}
+                            width={variables.iconBottomBar}
+                            height={variables.iconBottomBar}
+                        />
+                    </View>
+                    <Text
+                        style={[
+                            styles.textSmall,
+                            styles.textAlignCenter,
+                            styles.mt1Half,
+                            selectedTab === NAVIGATION_TABS.WORKSPACES ? styles.textBold : styles.textSupporting,
+                            styles.navigationTabBarLabel,
+                        ]}
+                    >
+                        {translate('common.workspaces')}
+                    </Text>
+                </PressableWithFeedback>
                 <NavigationTabBarAvatar
                     style={styles.navigationTabBarItem}
                     isSelected={selectedTab === NAVIGATION_TABS.SETTINGS}
                     onPress={showSettingsPage}
                 />
-                <View style={[styles.flex1, styles.navigationTabBarItem]}>
-                    <NavigationTabBarFloatingActionButton isTooltipAllowed={isTooltipAllowed} />
-                </View>
             </View>
         </>
     );