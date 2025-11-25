759c9e5d83fadcbafa47475979cad8be133140a5
Yehor Kharchenko <yehor.kharchenko@swmansion.com>
2025-11-25 12:02:01 +0100

    moved pagination to next step line

--- src/components/MoneyReportHeader.tsx
+++ src/components/MoneyReportHeader.tsx
@@ -1444,7 +1444,7 @@ function MoneyReportHeader({
 
     const showNextStepBar = shouldShowNextStep && !!optimisticNextStep?.message?.length;
     const showNextStepSkeleton = shouldShowNextStep && !optimisticNextStep && !!isLoadingInitialReportActions && !isOffline;
-    const shouldShowMoreContent = showNextStepBar || showNextStepSkeleton || !!statusBarProps || (isReportInSearch && !shouldDisplayNarrowMoreButton);
+    const shouldShowMoreContent = showNextStepBar || showNextStepSkeleton || !!statusBarProps || isReportInSearch;
 
     return (
         <View style={[styles.pt0, styles.borderBottom]}>
@@ -1484,12 +1484,6 @@ function MoneyReportHeader({
                                     shouldAlwaysShowDropdownMenu
                                 />
                             </View>
-                        )}
-                        {isReportInSearch && (
-                            <MoneyRequestReportNavigation
-                                reportID={moneyRequestReport?.reportID}
-                                shouldDisplayNarrowVersion={!shouldDisplayNarrowMoreButton}
-                            />
                         )}
                     </View>
                 )}
@@ -1535,10 +1529,10 @@ function MoneyReportHeader({
                             />
                         )}
                     </View>
-                    {isReportInSearch && !shouldDisplayNarrowMoreButton && (
+                    {isReportInSearch && (
                         <MoneyRequestReportNavigation
                             reportID={moneyRequestReport?.reportID}
-                            shouldDisplayNarrowVersion={shouldDisplayNarrowVersion}
+                            shouldDisplayNarrowVersion={!shouldDisplayNarrowMoreButton}
                         />
                     )}
                 </View>
