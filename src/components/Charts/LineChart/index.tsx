c1a77d6e3fe7b9194a5285b66f1030fc270e377c
borys3kk <szymon.banys01@wp.pl>
2026-01-27 11:54:48 +0100

    make index.tsx for pie and line chart react-compiler compatible

--- src/components/Charts/LineChart/index.tsx
+++ src/components/Charts/LineChart/index.tsx
@@ -5,14 +5,23 @@ import { WithSkiaWeb } from '@shopify/react-native-skia/lib/module/web';
 import colors from '@styles/theme/colors';
 import type { LineChartProps } from '@components/Charts/types';
 
+const getLineChartContent = () => import('./LineChartContent');
+
 function LineChart(props: LineChartProps) {
     return (
         <WithSkiaWeb
             opts={{ locateFile: (file: string) => `/${file}` }}
-            getComponent={() => import('./LineChartContent')}
+            getComponent={getLineChartContent}
             componentProps={props}
             fallback={
-                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: colors.productLight200, borderRadius: 16, padding: 20 }}>
+                <View style={{
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundColor: colors.productLight200,
+                    borderRadius: 16,
+                    padding: 20
+                }}>
                     <ActivityIndicator size="large" />
                 </View>
             }
