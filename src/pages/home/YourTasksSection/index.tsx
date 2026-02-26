b38c173eebd8b706ac420e157d02436cce221faf
war-in <war-in@users.noreply.github.com>
2026-02-26 11:26:56 +0100

    fix: add bottom padding to YourTasks widget

--- src/pages/home/YourTasksSection/index.tsx
+++ src/pages/home/YourTasksSection/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import WidgetContainer from '@components/WidgetContainer';
 import useLocalize from '@hooks/useLocalize';
+import useThemeStyles from '@hooks/useThemeStyles';
 import useSelfDMTasks from './hooks/useSelfDMTasks';
 import TaskItem from './TaskItem';
 
 function YourTasksSection() {
     const {translate} = useLocalize();
+    const styles = useThemeStyles();
     const {tasks, hasAnyTasks} = useSelfDMTasks();
 
     if (!hasAnyTasks) {
@@ -13,7 +15,10 @@ function YourTasksSection() {
     }
 
     return (
-        <WidgetContainer title={translate('homePage.yourTasks')}>
+        <WidgetContainer
+            title={translate('homePage.yourTasks')}
+            containerStyles={styles.pb5}
+        >
             {tasks.map((task) => (
                 <TaskItem
                     key={task.reportID}
