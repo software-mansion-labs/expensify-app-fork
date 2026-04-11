6e5bf5ae667b9686e006f896c75d766372317d4d
Dariusz Biela <dariusz.biela.dev@gmail.com>
2026-04-11 13:31:25 +0200

    refactor: stabilize parentPolicy ref in TransactionListItem via useStableValue

--- src/components/Search/SearchList/ListItem/TransactionListItem.tsx
+++ src/components/Search/SearchList/ListItem/TransactionListItem.tsx
@@ -83,7 +83,8 @@ function TransactionListItem<TItem extends ListItem>({
     // NOTE: Using || instead of ?? to treat empty string "" as falsy
     // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
     const policyID = transactionItem.policyID || snapshotReport?.policyID || activePolicyID;
-    const [parentPolicy] = originalUseOnyx(`${ONYXKEYS.COLLECTION.POLICY}${getNonEmptyStringOnyxID(policyID)}`);
+    const [unstableParentPolicy] = originalUseOnyx(`${ONYXKEYS.COLLECTION.POLICY}${getNonEmptyStringOnyxID(policyID)}`);
+    const parentPolicy = useStableValue(unstableParentPolicy);
     const unstableSnapshotPolicy = (currentSearchResults?.data?.[`${ONYXKEYS.COLLECTION.POLICY}${transactionItem.policyID}`] ?? {}) as Policy;
     const snapshotPolicy = useStableValue(unstableSnapshotPolicy);
 
